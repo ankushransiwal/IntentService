3f237a3fffc7ff3e8785d3796da19ccf5b8dba57
Ankush <ankushransiwal.13@gmail.com>
2018-07-13 21:07:32 +0530

    Use Broadcast Manager class to broadcast messages and recieve them anywhere

--- app/src/main/java/com/example/android/restful/services/MyService.java
+++ app/src/main/java/com/example/android/restful/services/MyService.java
@@ -3,12 +3,16 @@ package com.example.android.restful.services;
 import android.app.IntentService;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 
 public class MyService extends IntentService {
 
     public static final String TAG = "MyService";
+
+    public static final String MY_SERVICE_MSG = "myServiceMessage";
+    public static final String MY_SERVICE_PAYLOAD = "myServicePayload";
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
      *
@@ -29,6 +33,14 @@ public class MyService extends IntentService {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        Intent messageIntent = new Intent(MY_SERVICE_MSG);
+        messageIntent.putExtra(MY_SERVICE_PAYLOAD,"Service all done!");
+
+        //Make sure to include the one LocalBroadcastManager with the context one
+        LocalBroadcastManager manager =
+                LocalBroadcastManager.getInstance(getApplicationContext());
+        manager.sendBroadcast(messageIntent);
 
     }
 
